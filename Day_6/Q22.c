97fe886e146a784e42342d40b873d1705e1bc048
Kunal <kr4472694@gmail.com>
2026-06-02 23:53:17 +0530

    Day-6 Assignment

---
+++ Day_6/Q22.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    long long binary;
+    int decimal = 0, position = 0, digit;
+
+    printf("Enter a binary number: ");
+    scanf("%lld", &binary);
+
+    while (binary != 0)
+    {
+        digit = binary % 10;
+        decimal += digit * pow(2, position);
+
+        binary = binary/10;
+        position++;
+    }
+
+    printf("Decimal number = %d", decimal);
+
+    return 0;
+}
