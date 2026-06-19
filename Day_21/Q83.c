f23432d4b4d56cdc0929b2f9801b27a3f42bc782
Kunal <kr4472694@gmail.com>
2026-06-20 02:44:04 +0530

    Day-21 Assignment

---
+++ Day_21/Q83.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int i, vowels = 0, consonants = 0;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    for(i = 0; str[i] != '\0'; i++)
+    {
+        if(str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || 
+           str[i] == 'o' || str[i] == 'u' ||
+           str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || 
+           str[i] == 'O' || str[i] == 'U')
+        {
+            vowels++;
+        }
+        else
+        {
+            consonants++;
+        }
+    }
+
+    printf("Number of vowels = %d\n", vowels);
+    printf("Number of consonants = %d", consonants);
+
+    return 0;
+}
